a8c3573c2cd3def3818cdf5497c7cae237f26014
Äntor <ntormahmud789@gmail.com>
2024-05-18 14:27:18 +0600

    Upload String Value VowelCount & Revarse String And available Book check

---
+++ string.php
@@ -0,0 +1,10 @@
+<?php
+
+$strings = ["Hello", "World", "PHP", "Programming"];
+$vowels = '[aeiouAEIOU]';
+foreach ($strings as $string) {
+    $vowelCount = preg_match_all("/$vowels/", $string);
+    $reversedString = strrev($string);
+    echo "Original String: $string, Vowel Count: $vowelCount, Reversed String: $reversedString\n ";
+}
+?>
